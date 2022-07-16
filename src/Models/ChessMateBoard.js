7e7977cbd4b6c1c6bb74f0688a84b30fab814ac1
Omar Hawas <omarhawas94@gmail.com>
2022-07-16 18:22:39 -0400

    added logic for gameOver and check

--- src/Models/ChessMateBoard.js
+++ src/Models/ChessMateBoard.js
@@ -1,18 +1,17 @@
-import { Chess } from 'chess.js';
-const WHITE = 'w';
-const BLACK = 'b';
+import { Chess } from "chess.js";
+const WHITE = "w";
+const BLACK = "b";
 
 export default class ChessMateBoard {
-  constructor(whitePlayerName, blackPlayerName) {
-    this.chess = new Chess();
-    this.playerTurn = WHITE,
-
+  constructor(whitePlayerName, blackPlayerName, boardConfig) {
+    this.chess = new Chess(boardConfig);
+    this.playerTurn = WHITE;
     this.whitePlayerName = whitePlayerName;
     this.blackPlayerName = blackPlayerName;
-    this.message = 'Game Initialized';
+    this.message = "Game Initialized";
     this.history = [];
   }
-  
+
   getBoard() {
     return this.chess.board();
   }
@@ -20,7 +19,8 @@ export default class ChessMateBoard {
   getMoves(squareName) {
     const moves = this.chess.moves({ square: squareName });
 
-    const playerName = this.playerTurn === WHITE ? this.whitePlayerName : this.blackPlayerName; 
+    const playerName =
+      this.playerTurn === WHITE ? this.whitePlayerName : this.blackPlayerName;
     if (moves.length === 0) {
       this.message = "There are no possible moves for this piece.";
     } else {
@@ -35,9 +35,36 @@ export default class ChessMateBoard {
       this.message = "This is not a valid move!";
       return;
     }
-    
+
     const piece = move.piece;
-    this.history.push(`${this.playerTurn} moved a ${piece} from ${from} to ${to}`);
+    this.history.push(
+      `${this.playerTurn} moved a ${piece} from ${from} to ${to}`
+    );
     this.playerTurn = this.playerTurn === WHITE ? BLACK : WHITE;
+  }
+
+  inCheck() {
+    const inCheck = this.chess.in_check();
+    if (inCheck) {
+      this.message = `${this.playerTurn} is in check`;
+    }
+
+    return inCheck;
+  }
+
+  isGameOver() {
+    const gameOver = this.chess.game_over();
+    if (gameOver) {
+      this.message = "Game Ended";
+    }
+
+    return {
+      gameOver,
+      checkMate: this.chess.in_checkmate(),
+      draw: this.chess.in_draw(),
+      stalemate: this.chess.in_stalemate(),
+      threeFoldRepetition: this.chess.in_threefold_repetition(),
+      insufficientMaterial: this.chess.insufficient_material(),
+    };
   }
 }
--- src/Models/ChessMateBoard.test.js
+++ src/Models/ChessMateBoard.test.js
@@ -1,55 +1,130 @@
-import ChessMateBoard from './ChessMateBoard';
+import ChessMateBoard from "./ChessMateBoard";
 
-test('can initialize a board', () => {
+test("can initialize a board", () => {
   const player1 = "ahmed";
   const player2 = "omar";
 
   const chessMateBoard = new ChessMateBoard(player1, player2);
   expect(chessMateBoard.chess).toBeTruthy();
-  expect(chessMateBoard.playerTurn).toEqual('w');
+  expect(chessMateBoard.playerTurn).toEqual("w");
   expect(chessMateBoard.whitePlayerName).toEqual(player1);
   expect(chessMateBoard.blackPlayerName).toEqual(player2);
 });
 
-test('getBoard returns the board from the chessObject', () => {
-  const chessMateBoard = new ChessMateBoard('player1', 'player2');
+test("getBoard returns the board from the chessObject", () => {
+  const chessMateBoard = new ChessMateBoard("player1", "player2");
 
-  expect(chessMateBoard.getBoard()).toEqual(
-    [[{"color": "b", "square": "a8", "type": "r"}, {"color": "b", "square": "b8", "type": "n"}, {"color": "b", "square": "c8", "type": "b"}, {"color": "b", "square": "d8", "type": "q"}, {"color": "b", "square": "e8", "type": "k"}, {"color": "b", "square": "f8", "type": "b"}, {"color": "b", "square": "g8", "type": "n"}, {"color": "b", "square": "h8", "type": "r"}], [{"color": "b", "square": "a7", "type": "p"}, {"color": "b", "square": "b7", "type": "p"}, {"color": "b", "square": "c7", "type": "p"}, {"color": "b", "square": "d7", "type": "p"}, {"color": "b", "square": "e7", "type": "p"}, {"color": "b", "square": "f7", "type": "p"}, {"color": "b", "square": "g7", "type": "p"}, {"color": "b", "square": "h7", "type": "p"}], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [{"color": "w", "square": "a2", "type": "p"}, {"color": "w", "square": "b2", "type": "p"}, {"color": "w", "square": "c2", "type": "p"}, {"color": "w", "square": "d2", "type": "p"}, {"color": "w", "square": "e2", "type": "p"}, {"color": "w", "square": "f2", "type": "p"}, {"color": "w", "square": "g2", "type": "p"}, {"color": "w", "square": "h2", "type": "p"}], [{"color": "w", "square": "a1", "type": "r"}, {"color": "w", "square": "b1", "type": "n"}, {"color": "w", "square": "c1", "type": "b"}, {"color": "w", "square": "d1", "type": "q"}, {"color": "w", "square": "e1", "type": "k"}, {"color": "w", "square": "f1", "type": "b"}, {"color": "w", "square": "g1", "type": "n"}, {"color": "w", "square": "h1", "type": "r"}]]
+  expect(chessMateBoard.getBoard()).toEqual([
+    [
+      { color: "b", square: "a8", type: "r" },
+      { color: "b", square: "b8", type: "n" },
+      { color: "b", square: "c8", type: "b" },
+      { color: "b", square: "d8", type: "q" },
+      { color: "b", square: "e8", type: "k" },
+      { color: "b", square: "f8", type: "b" },
+      { color: "b", square: "g8", type: "n" },
+      { color: "b", square: "h8", type: "r" },
+    ],
+    [
+      { color: "b", square: "a7", type: "p" },
+      { color: "b", square: "b7", type: "p" },
+      { color: "b", square: "c7", type: "p" },
+      { color: "b", square: "d7", type: "p" },
+      { color: "b", square: "e7", type: "p" },
+      { color: "b", square: "f7", type: "p" },
+      { color: "b", square: "g7", type: "p" },
+      { color: "b", square: "h7", type: "p" },
+    ],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [
+      { color: "w", square: "a2", type: "p" },
+      { color: "w", square: "b2", type: "p" },
+      { color: "w", square: "c2", type: "p" },
+      { color: "w", square: "d2", type: "p" },
+      { color: "w", square: "e2", type: "p" },
+      { color: "w", square: "f2", type: "p" },
+      { color: "w", square: "g2", type: "p" },
+      { color: "w", square: "h2", type: "p" },
+    ],
+    [
+      { color: "w", square: "a1", type: "r" },
+      { color: "w", square: "b1", type: "n" },
+      { color: "w", square: "c1", type: "b" },
+      { color: "w", square: "d1", type: "q" },
+      { color: "w", square: "e1", type: "k" },
+      { color: "w", square: "f1", type: "b" },
+      { color: "w", square: "g1", type: "n" },
+      { color: "w", square: "h1", type: "r" },
+    ],
+  ]);
+});
+
+test("getMoves should return the valid moves", () => {
+  const chessMateBoard = new ChessMateBoard("player1", "player2");
+
+  expect(chessMateBoard.getMoves("e2")).toEqual(["e3", "e4"]);
+  expect(chessMateBoard.message).toEqual(
+    "These are your possible moves for e2, player1"
   );
 });
 
-test('getMoves should return the valid moves', () => {
-  const chessMateBoard = new ChessMateBoard('player1', 'player2');
+test("getMoves returns empty array if no valid moves", () => {
+  const chessMateBoard = new ChessMateBoard("player1", "player2");
 
-  expect(chessMateBoard.getMoves('e2')).toEqual(['e3', 'e4']);
-  expect(chessMateBoard.message).toEqual('These are your possible moves for e2, player1')
+  expect(chessMateBoard.getMoves("e1")).toEqual([]);
+  expect(chessMateBoard.message).toEqual(
+    "There are no possible moves for this piece."
+  );
 });
 
-test('getMoves returns empty array if no valid moves', () => {
-  const chessMateBoard = new ChessMateBoard('player1', 'player2');
-  
-  expect(chessMateBoard.getMoves('e1')).toEqual([]);
-  expect(chessMateBoard.message).toEqual('There are no possible moves for this piece.');
+test("makeMove returns message with invalid moves", () => {
+  const chessMateBoard = new ChessMateBoard("player1", "player");
+
+  expect(chessMateBoard.playerTurn).toEqual("w");
+
+  chessMateBoard.makeMove("e1", "e3");
+
+  expect(chessMateBoard.message).toEqual("This is not a valid move!");
+  expect(chessMateBoard.playerTurn).toEqual("w");
 });
 
-test('makeMove returns message with invalid moves', () => {
-  const chessMateBoard = new ChessMateBoard('player1', 'player');
+test("makeMove makes a move and adds to the history", () => {
+  const chessMateBoard = new ChessMateBoard("player1", "player2");
+  expect(chessMateBoard.playerTurn).toEqual("w");
 
-  expect(chessMateBoard.playerTurn).toEqual('w');
-  
-  chessMateBoard.makeMove('e1', 'e3');
-
-  expect(chessMateBoard.message).toEqual('This is not a valid move!');
-  expect(chessMateBoard.playerTurn).toEqual('w');
+  chessMateBoard.makeMove("e2", "e3");
+  expect(chessMateBoard.playerTurn).toEqual("b");
+  expect(chessMateBoard.history).toEqual(["w moved a p from e2 to e3"]);
 });
 
-test('makeMove makes a move and adds to the history', () => {
-  const chessMateBoard = new ChessMateBoard('player1', 'player2');
-  expect(chessMateBoard.playerTurn).toEqual('w');
+test("returns if in check", () => {
+  const chessMateBoard = new ChessMateBoard("player1", "player2");
+  chessMateBoard.makeMove("e2", "e4");
+  chessMateBoard.makeMove("f7", "f6");
+  chessMateBoard.makeMove("d1", "h5");
 
-  chessMateBoard.makeMove('e2', 'e3');
-  expect(chessMateBoard.playerTurn).toEqual('b');
-  expect(chessMateBoard.history).toEqual(['w moved a p from e2 to e3']);
+  const result = chessMateBoard.inCheck();
+  expect(chessMateBoard.message).toEqual(`b is in check`);
+  expect(result).toBeTruthy();
 });
 
+test("check if game is over", () => {
+  const chessMateBoard = new ChessMateBoard(
+    "player1",
+    "player2",
+    "4k3/4P3/4K3/8/8/8/8/8 b - - 0 78"
+  );
+  const result = chessMateBoard.isGameOver();
+  expect(chessMateBoard.message).toEqual("Game Ended");
+  expect(result).toEqual({
+    gameOver: true,
+    checkMate: false,
+    draw: true,
+    stalemate: true,
+    threeFoldRepetition: false,
+    insufficientMaterial: false,
+  });
+});
